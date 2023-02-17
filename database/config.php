10e8fc4007d9352fc9221350add14a823cc25dcd
mv-lb <125548341+mv-lb@users.noreply.github.com>
2023-02-17 15:08:29 +0530

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,6 +1,10 @@
 <?php
 
 // Configuration for database connection
+putenv('DB_HOST="hmc-velocity-server"');
+putenv('DB_USERNAME="hmcadmin"');
+putenv('DB_PASSWORD="hmc@1234"');
+putenv('DB_DATABASE="hmc-velocity-database"');
 
 $host       = getenv('DB_HOST');
 $username   = getenv('DB_USERNAME');
